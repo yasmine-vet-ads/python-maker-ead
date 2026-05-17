61f11fbc5b254a55309ca6acd55c76b2278409be
Yasmine Santos <yasmine.santos.vt@gmail.com>
2026-05-17 00:12:23 -0300

    Serve static files from 'static' directory
    Configured FastAPI to serve static files from the 'static' directory.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles # 1. Importe o módulo
 
 app = FastAPI()
+
+# 2. Diga ao FastAPI para servir os arquivos da pasta "static" na rota "/static"
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def get():
